d4daa5a1fc336d0ec6f3835901afea4e644944cb
Douglas Thor <doug.thor@gmail.com>
2023-11-04 21:47:57 -0700

    more move to pytest

--- tests/test_gdw.py
+++ tests/test_gdw.py
@@ -1,6 +1,5 @@
 import unittest
 from typing import Any
-from typing import Dict
 from typing import Tuple
 
 import pytest
@@ -102,8 +101,9 @@ def test_max_dist_squared_known_values(
     assert got == pytest.approx(want)
 
 
-class TestFlatLocation(unittest.TestCase):
-    known_values = (
+@pytest.mark.parametrize(
+    "diameter, want",
+    [
         (50, -23.7056196),
         (75, -35.8164473),
         (100, -47.2857008),
@@ -112,58 +112,52 @@ class TestFlatLocation(unittest.TestCase):
         (35, -17.5),
         (120, -60),
         (237.68, -118.84),
-    )
-
-    def test_known_values(self) -> None:
-        for dia, expected in self.known_values:
-            with self.subTest(diameter=dia):
-                result = gdw.flat_location(dia)
-                self.assertAlmostEqual(result, expected)
-
-    def test_invalid_input_raises_typeerror(self) -> None:
-        with self.assertRaises(TypeError):
-            gdw.flat_location("hello")  # type: ignore[arg-type]
+    ],
+)
+def test_flat_location_known_values(diameter: float, want: float) -> None:
+    got = gdw.flat_location(diameter)
+    assert got == pytest.approx(want)
 
 
-class TestGDWCalculation(unittest.TestCase):
-    known_values: Dict[
-        str,
-        Tuple[
-            Tuple[
-                Tuple[float, float],
-                float,
-                Tuple[gdw.OFFSET_TYPE, gdw.OFFSET_TYPE],
-                float,
-                float,
-            ],
-            int,
-        ],
-    ] = {
+def test_flat_location_invalid_input_raises_typeerror() -> None:
+    with pytest.raises(TypeError):
+        gdw.flat_location("hello")  # type: ignore[arg-type]
+
+
+@pytest.mark.parametrize(
+    "test_name, die_xy, diameter, offset_xy, excl, scribe_excl, want",
+    [
         # fmt: off
-        # name:   (((die_xy,      dia, offset_xy,      excl, scribe_excl, expected
-        "ints":   (((5, 5),       150, ('even', 'even'), 5,     5),       546),
-        "floats": (((5.0, 5.0),   150, ('even', 'even'), 5,     5),       546),
-        "t01":    (((3.34, 3.16), 100, ('even', 'even'), 5,     5),       548),
-        "t02-1":  (((2.43, 3.30), 150, ('even', 'odd'),  5,     4.5),     1814),
-        "t02-2":  (((2.43, 3.30), 150, ('even', 'even'), 5,     4.5),     1794),
-        "t02-3":  (((2.43, 3.30), 150, ('odd', 'odd'),   5,     4.5),     1800),
-        "t02-4":  (((2.43, 3.30), 150, ('odd', 'even'),  5,     4.5),     1804),
-        "t03":    (((4.34, 6.44), 150, ('even', 'even'), 5,     5),       484),
-        "t04":    (((1, 1),       150, ('even', 'even'), 5,     5),       14902),
-        "t05":    (((1, 1),       200, ('odd', 'even'),  5,     15),      27435),
-        "t06":    (((2.9, 3.3),   150, (-1.65, 2.95),    4.5,   4.5),     1529),
-        "t07":    (((2.69, 1.65), 150, (1.345, 2.1),     4.5,   4.5),     3346),
-        "t08":    (((4.4, 5.02),  150, (0, -0.2),        4.5,   4.5),     648),
+        # name,     die_xy,      dia, offset_xy,      excl, scribe_excl, expected
+        ("ints",    (5, 5),       150, ('even', 'even'), 5,     5,       546),
+        ("floats",  (5.0, 5.0),   150, ('even', 'even'), 5,     5,       546),
+        ("t01",     (3.34, 3.16), 100, ('even', 'even'), 5,     5,       548),
+        ("t02-1",   (2.43, 3.30), 150, ('even', 'odd'),  5,     4.5,     1814),
+        ("t02-2",   (2.43, 3.30), 150, ('even', 'even'), 5,     4.5,     1794),
+        ("t02-3",   (2.43, 3.30), 150, ('odd', 'odd'),   5,     4.5,     1800),
+        ("t02-4",   (2.43, 3.30), 150, ('odd', 'even'),  5,     4.5,     1804),
+        ("t03",     (4.34, 6.44), 150, ('even', 'even'), 5,     5,       484),
+        ("t04",     (1, 1),       150, ('even', 'even'), 5,     5,       14902),
+        ("t05",     (1, 1),       200, ('odd', 'even'),  5,     15,      27435),
+        ("t06",     (2.9, 3.3),   150, (-1.65, 2.95),    4.5,   4.5,     1529),
+        ("t07",     (2.69, 1.65), 150, (1.345, 2.1),     4.5,   4.5,     3346),
+        ("t08",     (4.4, 5.02),  150, (0, -0.2),        4.5,   4.5,     648),
         # fmt: on
-    }
-
-    def test_known_values(self) -> None:
-        for k, (v, expected) in self.known_values.items():
-            with self.subTest(test_name=k):
-                gdw_list = gdw.gdw(*v)
-                # count only die that are probed
-                result = sum(1 for x in gdw_list[0] if x[4] == "probe")
-                self.assertEqual(result, expected)
+    ],
+)
+def test_gdw_known_values(
+    test_name: str,
+    die_xy: Tuple[float, float],
+    diameter: float,
+    offset_xy: Tuple[gdw.OFFSET_TYPE, gdw.OFFSET_TYPE],
+    excl: float,
+    scribe_excl: float,
+    want: int,
+) -> None:
+    gdw_list = gdw.gdw(die_xy, diameter, offset_xy, excl, scribe_excl)
+    # count only die that are probed
+    got = sum(1 for x in gdw_list[0] if x[4] == "probe")
+    assert got == want
 
 
 @unittest.skip("tested function not completed yet")
